cec046d4aac3acc8c536ec0cc854bfb012d9236f
austin <austin.m.gray210@gmail.com>
2021-01-14 23:53:27 -0600

    finished dicee challenge

--- Dicee Challenge - Starting Files/js/index.js
+++ Dicee Challenge - Starting Files/js/index.js
@@ -1,7 +1,19 @@
 let randomNumber1 = Math.floor((Math.random() * 6 ) + 1 );
 let randomNumber2 = Math.floor((Math.random() * 6 ) + 1 );
 
-// let secondImage = document.getElementsByClassName("img2");
+let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
-console.log(firstImage);
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage1);
 
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "It's a tie!"
+};
