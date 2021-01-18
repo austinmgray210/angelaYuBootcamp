bde48ef5573ef0e63ab10bb86611d97e5a904dee
austin <austin.m.gray210@gmail.com>
2021-01-17 21:24:22 -0600

    commented out audio file

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,7 +3,13 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
+
+
+
+
     });
+
+    // let tom1 = new Audio("sounds/tom-1.mp3");
+    // tom1.play();
+
 }
