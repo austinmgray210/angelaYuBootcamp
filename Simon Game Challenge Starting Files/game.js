9c6566388cfd82fe67dc4425be88ec331c8b4dd5
austin <austin.m.gray210@gmail.com>
2021-01-24 20:48:31 -0600

    finished step 3 for simon says game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,6 +7,8 @@ function nextSequence() {
     let randomNumber = Math.floor((Math.random()) * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $("." + randomChosenColor).fadeOut().fadeIn();
+    new Audio("sounds/" + randomChosenColor + ".mp3").play();
     console.log(gamePattern);
 }
 
