5c94232a551a9dbd4ad8c94e099e7e26ae5d6629
austin <austin.m.gray210@gmail.com>
2021-01-25 13:49:20 -0600

    finished Step 4 of simon says

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,9 @@
 
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+
+
 
 function nextSequence() {
     let randomNumber = Math.floor((Math.random()) * 4);
@@ -11,5 +14,13 @@ function nextSequence() {
     new Audio("sounds/" + randomChosenColor + ".mp3").play();
     console.log(gamePattern);
 }
+
+// Checking to see which button was pressed.
+$(".btn").click(function (e) {
+    let userChosenColor = [];
+    userChosenColor.push(e.currentTarget.id);
+    userClickedPattern.push(e.currentTarget.id);
+    console.log(userClickedPattern);
+});
 
 
