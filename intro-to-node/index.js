2bca616fe85d2c5301bf80c8ee0ab3c9dba52901
austin <austin.m.gray210@gmail.com>
2021-01-28 20:45:54 -0600

    finished How to use Native Node Modules

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,3 +1,5 @@
-// We can type "node (specify file name)" to run files in the command line such as this file.
-// Ex.   node index.js    from this file will print "Hello World!" in the terminal
-console.log("Hello World!");
+//jshint esversion:6
+
+const fs = require("fs");
+
+fs.copyFileSync("file1.txt", "file2.txt");
