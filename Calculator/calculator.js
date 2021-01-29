df59237daf02fc518fc6837e1803650e19161069
austin <austin.m.gray210@gmail.com>
2021-01-29 15:59:50 -0600

    Finished Responding to Requests with HTML files

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
-    console.log("App started on localhost:3000");
+    console.log("Server is running on port 3000");
 });
