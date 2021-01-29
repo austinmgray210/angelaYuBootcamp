5c1e560921a7886bd47643d385992e58a4069808
austin <austin.m.gray210@gmail.com>
2021-01-28 19:48:40 -0600

    added intro-to-node folder

---
+++ intro-to-node/index.js
@@ -0,0 +1,3 @@
+// We can type "node (specify file name)" to run files in the command line such as this file.
+// Ex.   node index.js    from this file will print "Hello World!" in the terminal
+console.log("Hello World!");
