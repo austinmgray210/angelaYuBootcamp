f45e2ace1ab1b205e3a55a05a75bc86c0ba3c07a
austin <austin.m.gray210@gmail.com>
2021-01-29 15:22:41 -0600

    finished Understanding and Working with Routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Root page
 app.get("/", (req, res) => {
     res.send("<h1>Hello, world!</h1>");
+});
+
+// Contacts page
+app.get("/contact", (req, res) => {
+   res.send("Contact Me at: Austin@gmail.com");
+});
+
+// About Me page
+app.get("/about", (req, res) => {
+   res.send("This website is owned by Austin Gray");
+});
+
+app.get("/hobbies", (req, res) => {
+   res.send("<ul><li>Rootbeer</li><li>Code</li><li>Sleep</li></ul>")
 });
 
 app.listen(port, function () {
