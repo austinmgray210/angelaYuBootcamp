3950af4d7ebeb5dfb3a0ab61d2150fc24f44a49d
austin <austin.m.gray210@gmail.com>
2021-01-29 14:57:05 -0600

    added Creating Our First Server with Express files

--- intro-to-node/index.js
+++
@@ -1,8 +0,0 @@
-//jshint esversion:6
-
-const superheroes = require("superheroes");
-const supervillains = require("supervillains");
-
-let mySuperheroName = superheroes.random();
-let mySuperVillainName = supervillains.random();
-console.log(mySuperVillainName);
