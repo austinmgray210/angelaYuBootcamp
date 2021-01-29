d58e93aae99c8d46b035aa496c2b1eca0bc87947
austin <austin.m.gray210@gmail.com>
2021-01-29 15:12:06 -0600

    finished Handling Requests and Response: The GET Request

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, world!</h1>");
+});
+
 app.listen(port, function () {
     console.log("Server started on port 3000");
 });
