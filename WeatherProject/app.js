efe2336d0bbcf8feaf7f34aa6a187ff857522eeb
austin <austin.m.gray210@gmail.com>
2021-02-01 19:49:03 -0600

    hiding API key

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const app = express();
 
 
+
 app.get("/", (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&units=imperial&appid="//Insert Key;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&units=imperial&appid=" + process.env.API_KEY;
 
     https.get(url, (response) => {
         console.log(response.statusCode);
