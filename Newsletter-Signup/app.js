b81c88ae0121fd1a038aada143da2130539c6884
austin <austin.m.gray210@gmail.com>
2021-02-02 21:25:54 -0600

    Started Newsletter-Signup project

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.send("Hello there");
+});
+
+
+app.post('/', (req, res) => {
+  res.send("Hello");
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.")
+});
