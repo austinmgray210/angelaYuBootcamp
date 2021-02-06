d8c8a7dffce7a01c5fdb17f9f3e41195baa4ac74
austin <austin.m.gray210@gmail.com>
2021-02-05 23:10:36 -0600

    code cleanup

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -11,18 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/signup.html")
 });
 
-
 app.post('/', (req, res) => {
     let fistName = req.body.fName;
     let lastName = req.body.lName;
     let email = req.body.email;
     console.log(fistName, lastName, email);
-
-
-
 });
-
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.")
