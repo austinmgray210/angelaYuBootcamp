381398170e72c83cad41da2cb0734415e39f4ec4
austin <austin.m.gray210@gmail.com>
2021-03-06 19:22:05 -0600

    added mailchimp api functionality

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
+require("dotenv").config();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -12,10 +13,41 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    let fistName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
-    console.log(fistName, lastName, email);
+    const fistName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fistName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const apiKey = process.env.API_KEY;
+    const listKey = process.env.LIST_ID;
+    const url = "https://us1.api.mailchimp.com/3.0/lists/" + listKey;
+    const options = {
+        method: "POST",
+        auth: "austin1:" + apiKey
+    }
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log((JSON.parse(data)));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+
 });
 
 app.listen(3000, () => {
